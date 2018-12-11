8447c622a0bd97fd992556217b725600b0c253ca
Kaloli <rvellark@gmail.com>
2018-12-10 18:59:51 -0700

    newupdate

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -1,21 +1,28 @@
+$(document).ready() 
+
+
+
 var targetNumber;
 var counter = 0;
+var winner = 0;
+var hasWon = false;
+var hasLost = false;
+
 
 targetNumber = Math.floor(Math.random() * 69) + 30;
 console.log(targetNumber);
+
 $("#number-to-guess").html('Guess This Number: ' + targetNumber);
-
-
-
 $(".crystal-image1", ).on("click", function () {
-
-
 
   counter += 5;
   console.log(counter);
   $("#counterToGuess").text(counter);
+
   if (counter === targetNumber) {
-    alert("You win!");
+    winner += 1;
+
+    $("#gameWins").text(winner);
   } else if (counter >= targetNumber) {
     alert("You lose!!");
   }
@@ -24,11 +31,12 @@ $(".crystal-image1", ).on("click", function () {
 $(".crystal-image2", ).on("click", function () {
 
 
-  counter += 1;
+  counter += 4;
   console.log(counter);
   $("#counterToGuess").text(counter);
   if (counter === targetNumber) {
-    alert("You win!");
+    winner += 1;
+    $("#gameWins").text(winner);
   } else if (counter >= targetNumber) {
     alert("You lose!!");
   }
@@ -38,10 +46,23 @@ $(".crystal-image2", ).on("click", function () {
 $(".crystal-image3", ).on("click", function () {
 
 
-  counter += 5;
+  counter += 6;
   console.log(counter);
   $("#counterToGuess").text(counter);
+  if (counter === targetNumber) {
+    winner += 1;
+    $("#gameWins").text(winner);
+  } else if (counter >= targetNumber) {
+    alert("You lose!!");
+  }
 
+});
+$(".crystal-image4", ).on("click", function () {
+
+
+  counter += 3;
+  console.log(counter);
+  $("#counterToGuess").text(counter);
   if (counter === targetNumber) {
     alert("You win!");
   } else if (counter >= targetNumber) {
