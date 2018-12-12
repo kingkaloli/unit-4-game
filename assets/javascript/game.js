228fa422090f988b18ce5d1f5d6455e692b3e4e4
Kaloli <rvellark@gmail.com>
2018-12-12 09:39:28 -0700

    update12

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,88 @@
-$(document).ready()
+$(document).ready(function () {
 
 
 
-var targetNumber;
-var counter = 0;
-var winner = 0;
-var hasWon = false;
-var hasLost = false;
+  var targetNumber;
+  var counter = 0;
+  var winner = 0;
+  var hasWon = false;
+  var hasLost = false;
+
+  // I added this function to run everytime the player wins or loses, it will recalculate your random number and reset your counter
+  function startGame() {
+    targetNumber = Math.floor(Math.random() * 69) + 30;
+
+    //---Counter 1 and so forth 
+    console.log(targetNumber);
+    $("#number-to-guess").html('Guess This Number: ' + targetNumber)
+    counter = 0;
+    $("#counterToGuess").text(counter);
+  };
+
+  targetNumber = Math.floor(Math.random() * 69) + 30;
+  $("#number-to-guess").html('Guess This Number: ' + targetNumber)
+  $(".crystal-image1", ).on("click", function () {
+    counter += 5;
+
+    ////--define eeach counter 
+    console.log(counter);
+    $("#counterToGuess").text(counter);
+    if (counter === targetNumber) {
+      // I cahnged this to winner++ to increment the variable everytime you win
+      winner++ ;
+      $("#gameWins").text(winner);
+      // I called this function here to reset the game after you win
+      startGame();
+    } else if (counter >= targetNumber) {
+      alert("You lose!!");
+      // Samething here to restart your game after you lose
+      startGame();
+    }
 
 
-targetNumber = Math.floor(Math.random() * 69) + 30;
-console.log(targetNumber);
+  });
 
-$("#number-to-guess").html('Guess This Number: ' + targetNumber);
-$(".crystal-image1", ).on("click", function () {
+  $(".crystal-image2", ).on("click", function () {
+    counter += 4;
+    console.log(counter);
+    $("#counterToGuess").text(counter);
+    if (counter === targetNumber) {
+      winner++ ;
+      $("#gameWins").text(winner);
+      startGame();
+    } else if (counter >= targetNumber) {
+      alert("You lose!!");
+      startGame();
+    }
 
-  counter += 5;
-  console.log(counter);
-  $("#counterToGuess").text(counter);
+  });
 
-  if (counter === targetNumber) {
-    winner += 1;
-    $("#gameWins").text(winner);
-  } else if (counter >= targetNumber) {
-    alert("You lose!!");
-  }
+  $(".crystal-image3", ).on("click", function () {
+    counter += 6;
+    console.log(counter);
+    $("#counterToGuess").text(counter);
+    if (counter === targetNumber) {
+      winner++;
+      $("#gameWins").text(winner);
+      startGame();
+    } else if (counter >= targetNumber) {
+      alert("You lose!!");
+      startGame();
+    }
 
-
-});
-
-$(".crystal-image2", ).on("click", function () {
-
-
-  counter += 4;
-  console.log(counter);
-  $("#counterToGuess").text(counter);
-  if (counter === targetNumber) {
-    winner += 1;
-    $("#gameWins").text(winner);
-  } else if (counter >= targetNumber) {
-    alert("You lose!!");
-  }
-
-});
-
-$(".crystal-image3", ).on("click", function () {
-
-
-  counter += 6;
-  console.log(counter);
-  $("#counterToGuess").text(counter);
-  if (counter === targetNumber) {
-    winner += 1;
-    $("#gameWins").text(winner);
-  } else if (counter >= targetNumber) {
-    alert("You lose!!");
-  }
-
-});
-$(".crystal-image4", ).on("click", function () {
-
-
-  counter += 3;
-  console.log(counter);
-  $("#counterToGuess").text(counter);
-  if (counter === targetNumber) {
-    alert("You win!");
-  } else if (counter >= targetNumber) {
-    alert("You lose!!");
-  }
-
+  });
+  $(".crystal-image4", ).on("click", function () {
+    counter += 3;
+    console.log(counter);
+    $("#counterToGuess").text(counter);
+    if (counter === targetNumber) {
+      alert("You win!");
+      winner++;
+      $("#gameWins").text(winner);
+      startGame();
+    } else if (counter >= targetNumber) {
+      alert("You lose!!");
+      startGame();
+    }
+  })
 });
